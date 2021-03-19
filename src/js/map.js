6c9bedf0dc204d24bf95d14086a6d23bf2551fe9
Pavel Frolov <Frolov-Works@yandex.ru>
2021-03-19 21:32:26 +0300

    add price change from size, load map by scroll,favicon

--- src/js/map.js
+++ src/js/map.js
@@ -1,28 +1,55 @@
 ;(function() {
-    if(typeof ymaps === 'undefined') {
-        return
+    let sectionContacts = document.querySelector('.section-contacts')
+
+    let ymapInit = () => {
+        if(typeof ymaps === 'undefined') {
+            return
+        }
+
+        ymaps.ready(function () {
+            var myMap = new ymaps.Map('ymap', {
+                    center: [48.701579, 44.516517],
+                    zoom: 12
+                }, {
+                    searchControlProvider: 'yandex#search'
+                }),
+
+                myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                    hintContent: 'Собственный значок метки',
+                    balloonContent: 'г. Волгоград, набережная 62-й Армии, 1'
+                }, {
+                    iconLayout: 'default#image',
+                    iconImageHref: "./assets/marker.svg",
+                    iconImageSize: [40, 63.2],
+                    iconImageOffset: [-25, -50]
+                })
+
+            myMap.geoObjects.add(myPlacemark);
+            myMap.behaviors.disable('scrollZoom');
+
+        });
     }
 
-    ymaps.ready(function () {
-        var myMap = new ymaps.Map('ymap', {
-                center: [48.701579, 44.516517],
-                zoom: 12
-            }, {
-                searchControlProvider: 'yandex#search'
-            }),
+    let ymapLoad = () => {
+        let body = document.querySelector("body");
+        let script = document.createElement('script')
+        script.src = 'https://api-maps.yandex.ru/2.1/?lang=ru_RU'
+        body.appendChild(script)
+        script.addEventListener('load', ymapInit)
+    }
 
-            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-                hintContent: 'Собственный значок метки',
-                balloonContent: 'г. Волгоград, набережная 62-й Армии, 1'
-            }, {
-                iconLayout: 'default#image',
-                iconImageHref: "./assets/marker.svg",
-                iconImageSize: [40, 63.2],
-                iconImageOffset: [-25, -50]
-            })
+    let checkYmapInit = () => {
+        let sectionContactsTop = sectionContacts.getBoundingClientRect().top
+        let scrollTop = window.pageYOffset;
+        let sectionContactsOffsetTop = sectionContactsTop + scrollTop
 
-        myMap.geoObjects.add(myPlacemark);
-        myMap.behaviors.disable('scrollZoom');
+        if(scrollTop + window.innerHeight > sectionContactsOffsetTop){
+            ymapLoad()
+            window.removeEventListener('scroll', checkYmapInit)
+        }
+    }
 
-    });
+
+    window.addEventListener('scroll', checkYmapInit)
+    checkYmapInit()
 })();
